fcf8c9fa7901edd683a114cd3f084aee74c2c2cd
Matheus Andreoni <matheusandreoni97@gmail.com>
2020-09-18 08:38:13 -0300

    Initial structure

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Todo App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Todo List"),
+      ),
+      body: Container(
+        child: Text("Olá Mundo"),
+      ),
+    );
+  }
+}
